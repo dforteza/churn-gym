4899845848583ffe1845ce67033e3502108961ae
dforteza <dfortezabenito@gmail.com>
2026-04-07 20:22:36 +0200

    feat: añadir entidades JPA (Usuario, Cliente, Importacion, Visita, MetricaCliente)

---
+++ backend/ChurnGym/src/test/java/com/juandelacierva/ChurnGym/ChurnGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juandelacierva.ChurnGym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurnGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
