c7cde06d7ff9a2df1ef1aa69e68061c4f654c023
dforteza <dfortezabenito@gmail.com>
2026-04-08 20:48:04 +0200

    feat: añadir excepciones globales y handler

---
+++ backend/ChurnGym/src/main/java/com/juandelacierva/ChurnGym/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.juandelacierva.ChurnGym.exception;
+
+public class ResourceNotFoundException extends RuntimeException 
+{
+    public ResourceNotFoundException(String message) 
+    {
+        super(message);
+    }
+}
