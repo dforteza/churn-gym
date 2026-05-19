b36125bf7abf574e894459abcd27edfb7a02b31d
dforteza <dfortezabenito@gmail.com>
2026-05-19 21:38:04 +0200

    fix: actualizar username del admin en V5

--- backend/ChurnGym/src/main/resources/db/migration/V5__insert_admin.sql
+++ backend/ChurnGym/src/main/resources/db/migration/V5__insert_admin.sql
@@ -3,8 +3,8 @@
 
 INSERT INTO usuarios (username, email, password, rol, nombre_gimnasio, direccion1, direccion2, telefono, created_at, updated_at)
 VALUES (
-    'admin',
-    'admin@churngym.dev',
+    'diego',
+    'diego@churngym.dev',
     '$2a$10$sjlCpieBIMUkj3ei42oFae4dO5ouRKAZgfUwMvfxH3H8nrhUWLNyC',
     'ADMIN',
     'Churn Gym Madrid Centro',
